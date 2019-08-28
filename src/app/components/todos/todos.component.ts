bdd95f5b9013c3a85e2333a715a72080e7421346
Georgiana Negrea <ngeorgiana@icloud.com>
2019-08-28 16:33:51 +0300

    created add-todo comp, service and method for submit

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -29,4 +29,9 @@ export class TodosComponent implements OnInit {
     this.todoService.deleteTodo(todo).subscribe();
   }
 
+  addTodo(todo:Todo) {
+    this.todoService.addTodo(todo).subscribe(todo => {
+      this.todos.push(todo);
+    })
+  }
 }
